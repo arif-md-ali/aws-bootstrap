167c2612f6af314fcf13a9c529dee2f0de55ad02
arif <arif_md_ali@hotmail.com>
2021-02-27 20:11:50 +0530

    This is checking from the MacBook

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	console.log("===============Reached Server================== \n");
+	console.log("===============Reached Server===================== \n");
 	res.end(message);
 });
 
