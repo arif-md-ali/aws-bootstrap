cf4fa69a95b216831985a6b83e8fc0479974a2a0
Arif <arif_md_ali@hotmail.com>
2021-03-14 00:42:36 +0530

    checking new machine

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ const server = http.createServer((req, res) => {
 
 //server.listen(port, hostname, backlog, callback);
 server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname()}:${port}/`);
+	console.log(`Server up and running at http://${hostname()}:${port}/`);
 });
